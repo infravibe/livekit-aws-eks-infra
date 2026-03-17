2be9369656838470d9d85e02a1941d52b033adf5
Akash Sahani <sahaniakash48@gmail.com>
2026-03-17 15:18:41 +0530

    Create acm.tf

---
+++ terraform/acm.tf
@@ -0,0 +1,27 @@
+# acm.tf
+
+resource "aws_acm_certificate" "livekit" {
+  domain_name               = "livekit.yourdomain.com"
+  subject_alternative_names = ["turn.yourdomain.com"]
+  validation_method         = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.livekit.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = each.value.name
+  type    = each.value.type
+  ttl     = 60
+  records = [each.value.record]
+}
